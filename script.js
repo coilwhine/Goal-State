11fdb987b96f1061ecef2e64276027537d13e535
Liormym <liormym1@gmail.com>
2022-09-03 16:48:57 +0300

    Update script.js
    new function statistics

--- script.js
+++ script.js
@@ -3,64 +3,64 @@ const apiKey = "ca6d66a0a93082ed6d39d08eac0f7e8b3df170738be465f6e4a356ce414b9838
 // getting all live games 
 
 
-function fetchFromApi(){
+function fetchFromApi() {
 
     fetch(`https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=${apiKey}`)
-    .then((res) => res.json())
-    .then((val) => {
-        console.log(val.result[0])
+        .then((res) => res.json())
+        .then((val) => {
+            console.log(val.result[0])
 
-        let img = document.createElement("img")
-        img.src = val.result[0].home_team_logo;
-        document.body.append(img)
-        
-        let score = document.createElement("span")
-        score.innerText = val.result[0].event_final_result;
-        document.body.append(score)
-        
-        let img2 = document.createElement("img")
-        img2.src = val.result[0].away_team_logo;
-        document.body.append(img2)
-    });
-    
-    
-    
+            let img = document.createElement("img")
+            img.src = val.result[0].home_team_logo;
+            document.body.append(img)
+
+            let score = document.createElement("span")
+            score.innerText = val.result[0].event_final_result;
+            document.body.append(score)
+
+            let img2 = document.createElement("img")
+            img2.src = val.result[0].away_team_logo;
+            document.body.append(img2)
+        });
+
+
+
     // getting all games in next 24 HOURS
-    
-    
+
+
     let date = new Date();
     let year = date.getFullYear();
     let month = date.getMonth() + 1;
-    if(month < 10){
+    if (month < 10) {
         month = "0" + month
     }
     let day = date.getDate();
-    if(day < 10){
+    if (day < 10) {
         day = "0" + day;
     }
-    
-    const startDateFrom = [year, month,  day].join('-');
+
+    const startDateFrom = [year, month, day].join('-');
     console.log(startDateFrom); // 👉️ 2022-10-25
-    
-    
+
+
     let tomorrow = Number.parseFloat(day) + 1;
-    if (tomorrow < 10){
+    if (tomorrow < 10) {
         tomorrow = "0" + tomorrow;
     }
-    
+
     const endDate = [year, month, tomorrow].join('-');
     console.log(endDate); // 👉️ 2022-10-26
-    
+
     fetch(`https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=${apiKey}&from=${startDateFrom}&to=${endDate}`)
-    .then((res) => res.json())
-.then((val) => {
-    console.log(val)
-}).catch((e) => console.log(e))
+        .then((res) => res.json())
+        .then((val) => {
+            console.log(val)
+        }).catch((e) => console.log(e))
 
 }
 
 
-    // getting player image by ID (will used to live game goalscores)
+// getting player image by ID (will used to live game goalscores)
 
 
 async function returnPic(id) {
@@ -68,4 +68,30 @@ async function returnPic(id) {
     const playerInfo = await result.json();
     let playerImage = playerInfo.result[0].player_image;
     return playerImage;
+}
+
+function fetchFromApiStatistics(index) {
+
+    fetch(`https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=${apiKey}`)
+        .then((res) => res.json())
+        .then((val) => {
+            console.log(val.result[0])
+
+            const homeSub = val.result[index].statistics[1].home;
+            const awaySub = val.result[index].statistics[1].away;
+            const homeTotalShots = val.result[index].statistics[6].home;
+            const AwayTotalShots = val.result[index].statistics[6].away;
+            const homeShotsOnTarget = val.result[index].statistics[7].home;
+            const awayShotsOnTarget = val.result[index].statistics[7].away;
+            const homeShotsOffTarget = val.result[index].statistics[8].home;
+            const awayShotsOffTarget = val.result[index].statistics[8].away;
+            const homeCorners = val.result[index].statistics[13].home;
+            const awayCorners = val.result[index].statistics[13].away;
+            const homeBallPossession = val.result[index].statistics[14].home;
+            const awayBallPossession = val.result[index].statistics[14].away;
+            const homeYellowCards = val.result[index].statistics[15].home;
+            const awayYellowCards = val.result[index].statistics[15].away;
+            const homeSaves = val.result[index].statistics[16].home;
+            const awaySaves = val.result[index].statistics[16].away;
+        })
 }
